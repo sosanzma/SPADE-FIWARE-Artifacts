1f5b833248fd11b948c86d8f2f5fd988006c8334
manel <manelbng@gmail.com>
2024-09-04 12:01:09 +0200

    actions test 4

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name="spade_fiware_artifacts",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(exclude=["test*"]),
 )
